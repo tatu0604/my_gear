ba0fd587fefd3d0f6d7c52b9a8272d3904e9fd42
tatu0604 <26532653h@gmail.com>
2021-08-03 16:09:57 +0900

    詳細編集ページ実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,9 +17,11 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
