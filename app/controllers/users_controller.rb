9882925136b4fac7dc5a948cbd8cc08293b32751
tatu0604 <26532653h@gmail.com>
2021-08-13 16:29:18 +0900

    ビュー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @profile = @user.profile
+    @posts = @user.posts
   end
 end
