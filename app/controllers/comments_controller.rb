ca7fe0f607d8671faa3134ec788701c129199641
tatu0604 <26532653h@gmail.com>
2021-08-24 17:44:44 +0900

    ビュー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
