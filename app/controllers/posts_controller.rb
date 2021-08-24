82a8e7e1736266914d218bad5c364604ff6ca72f
tatu0604 <26532653h@gmail.com>
2021-08-24 16:57:28 +0900

    リダイレクト修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
   before_action :set_posts, only: [:show, :edit, :update, :destroy]
 
   def index
